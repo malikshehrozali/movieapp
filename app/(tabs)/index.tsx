287949e12e00d1068df86cdf6198123109302e6c
malikshehrozali <malikshehrozali16@gmail.com>
2026-03-21 23:39:51 +0500

    added the first UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-black font-semibold">Welcome</Text>
+    <View className="bg-primary flex-1">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <SearchBar />
+      </ScrollView>
     </View>
   );
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SearchBar() {
+  return (
+    <View>
+      <Text className="text-white">This Is the search bar</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
